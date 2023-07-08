ded5a661baaf80523fb2669d0675d1a4f72dfe70
Alberth Cruz Matos <alberth@matos.cc>
2023-07-08 00:30:01 -0400

    Changes to support LGX

--- Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
+++ Firmware/variants/1_75mm_MK3S-EINSy10a-E3Dv6full.h
@@ -17,7 +17,7 @@
 #define NOZZLE_TYPE "E3Dv6full"
 
 // Printer name
-#define CUSTOM_MENDEL_NAME "Prusa i3 MK3S"
+#define CUSTOM_MENDEL_NAME "BT-Mos MK3S+ PT1k"
 
 // Electronics
 #define MOTHERBOARD BOARD_EINSY_1_0a
@@ -25,7 +25,7 @@
 #define HAS_SECOND_SERIAL_PORT
 
 // PSU
-// #define PSU_Delta                                 // uncomment if DeltaElectronics PSU installed
+#define PSU_Delta                                 // uncomment if DeltaElectronics PSU installed
 
 
 // Uncomment the below for the E3D PT100 temperature sensor (with or without PT100 Amplifier)
@@ -44,7 +44,8 @@
 
 // Steps per unit {X,Y,Z,E}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,140}
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,280}
+// #define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,280} // Default for Prusa
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,400}
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {100,100,3200/8,560}
 
 // Endstop inverting
@@ -220,7 +221,7 @@
 
 #define TMC2130_USTEPS_XY   16        // microstep resolution for XY axes
 #define TMC2130_USTEPS_Z    16        // microstep resolution for Z axis
-#define TMC2130_USTEPS_E    32        // microstep resolution for E axis
+#define TMC2130_USTEPS_E    16        // microstep resolution for E axis
 #define TMC2130_INTPOL_XY   1         // extrapolate 256 for XY axes
 #define TMC2130_INTPOL_Z    1         // extrapolate 256 for Z axis
 #define TMC2130_INTPOL_E    1         // extrapolate 256 for E axis
